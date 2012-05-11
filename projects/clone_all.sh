1da96d7f613a388ab938ecfe72fb468858420bb3
Senthil Nayagam <senthil@railsfactory.org>
2012-05-11 18:04:25 +0530

    just realised there was a bug in the shell script, fixed it and updated the projects

--- projects/clone_all.sh
+++ projects/clone_all.sh
@@ -1,6 +1,6 @@
 cat projects.list | while read line ; do
    echo === $line ===
-mydirectory=( $(echo $line | cut -d'/' -f5))
+mydirectory=( $(echo $line | cut -d'/' -f5 | sed -e 's/.git//'))
 if [ ! -d "$mydirectory" ]; then
 	echo git clone $line
 	 git clone $line
